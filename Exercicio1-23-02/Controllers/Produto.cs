2b84fc579810b25bbae44ab1ec0eb4a56305b4a1
0220832111001 <0220832111001@LAB4TC26>
2022-02-23 11:05:28 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio1-23-02/Controllers/Produto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exercicio1_23_02.Controllers
+{
+    public class _ : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult cadastrar()
+        {
+            return View();
+        }
+    }
+}
---
+++ Exercicio1-23-02/Models/Produto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exercicio1_23_02.Models
+{
+    public class Produto
+    {
+        public string nome { get; set; }
+        public string descricao { get; set; }
+        public float valor { get; set; }
+    }
+}
