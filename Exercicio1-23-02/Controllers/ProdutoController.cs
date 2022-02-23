6f8e7cfcbc432d381a71d97157d0949b31261002
MARIA CLARA GANDINI PEREIRA <maria.pereira53@fatec.sp.gov.br>
2022-02-23 11:15:18 -0300

    terminar

---
+++ Exercicio1-23-02/Controllers/ProdutoController.cs
@@ -0,0 +1,29 @@
+﻿using Exercicio1_23_02.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exercicio1_23_02.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public static List<Produto> lsProduto = new List<Produto>();
+        public IActionResult Index()
+        {
+            return View(lsProduto);
+        }
+        public IActionResult cadastrar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult salvar(Produto item)
+        {
+            lsProduto.Add(item);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
